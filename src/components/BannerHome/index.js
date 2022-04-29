18f1d27d24be370c7b1aba2638c2ebb6320a7ab7
melaniemdm <melanieborja.pro@gmail.com>
2022-04-29 10:31:50 +0200

    feature(js) composant banner home et banner apropos

---
+++ src/components/BannerHome/index.js
@@ -0,0 +1,14 @@
+import bannerHomeImg from '../../assets/bannerHome.png';
+import Banner from '../../layout/Banner';
+
+function BannerHome() {
+    const txt = "Chez vous, partout et ailleurs";
+    return <div id='cardBanner'>
+        
+        
+            <Banner img={bannerHomeImg} txt={txt}/> 
+        </div>
+
+}
+
+export default BannerHome
---
+++ src/components/BannerPropos/index.js
@@ -0,0 +1,14 @@
+import bannerAProposImg from '../../assets/bannerAPropos.png';
+import Banner from '../../layout/Banner';
+
+function BannerPropos() {
+  
+    return <div id='cardBanner'>
+        
+        
+            <Banner img={bannerAProposImg} txt=""/> 
+        </div>
+
+}
+
+export default BannerPropos
