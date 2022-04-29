3ebb3cc0e716c3e0e7ecf71c8050932b59ec539a
melaniemdm <melanieborja.pro@gmail.com>
2022-04-29 15:37:22 +0200

    fix(css) police fix(js) lien index.scss

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
-import './index.css';
+import '../src/styles/index.scss';
 //import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Header from '../src/layout/Header';
