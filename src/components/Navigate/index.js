8bab87718caf6dc5dce1340cf4d216a14778b68b
melaniemdm <melanieborja.pro@gmail.com>
2022-05-16 10:40:15 +0200

    fix(js) space delete

--- src/components/Navigate/index.js
+++ src/components/Navigate/index.js
@@ -6,7 +6,7 @@ function Navigate(){
 
   return <div className="containerNavigate">
     <nav className="navigate">
-      <Link to="/home " className="accueil">Accueil</Link> 
+      <Link to="/home" className="accueil">Accueil</Link> 
       <Link to="/propos" className="apropos">A propos</Link>
     </nav>
   </div>;
